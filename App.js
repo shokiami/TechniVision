5030a865b0a8f8cd8a44ef1e43f8a4e6ff8aee15
shokiami <kiami.sho@gmail.com>
2020-04-06 17:46:00 -0700

    TensorFlow implemented

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-react-native';
 import {Camera} from 'expo-camera';
+import {cameraWithTensors} from '@tensorflow/tfjs-react-native';
+
+const TensorCamera = cameraWithTensors(Camera);
 
 export default class App extends Component {
   state = {
+    loadedTensorFlow: false,
     cameraPermissions: false,
-    type: Camera.Constants.Type.back
+    type: Camera.Constants.Type.front
   };
 
   async componentDidMount() {
     await this.getCameraPermission();
+    await this.loadTensorFlow();
   }
 
   async getCameraPermission() {
@@ -17,9 +24,48 @@ export default class App extends Component {
     this.setState({cameraPermissions: (permission.status == 'granted')});
   };
 
+  async loadTensorFlow() {
+    await tf.ready();
+    this.setState({loadedTensorFlow: true});
+  };
+
+  handleCameraStream(images, updatePreview, gl) {
+    const loop = async () => {
+      const nextImageTensor = images.next().value
+
+      //
+      // do something with tensor here
+      //
+
+      // if autorender is false you need the following two lines.
+      // updatePreview();
+      // gl.endFrameEXP();
+
+      requestAnimation(loop);
+    }
+    loop();
+  }
+
   render() {
-    console.log(this.state);
-    if (!this.state.cameraPermissions) {
+    let textureDims;
+    if (Platform.OS === 'ios') {
+      textureDims = {
+        height: 1920,
+        width: 1080,
+      };
+    } else {
+      textureDims = {
+        height: 1200,
+        width: 1600,
+      };
+    }
+    if (!this.state.loadedTensorFlow) {
+      return (
+        <View>
+          <Text>Loading TensorFlow...</Text>
+        </View>
+      );
+    } else if (!this.state.cameraPermissions) {
       return (
         <View>
           <Text>Camera Permission Denied.</Text>
@@ -28,12 +74,18 @@ export default class App extends Component {
     } else {
       return (
         <View style={styles.container}>
-          <Camera
-            ref={cam => {
-              this.camera = cam;
-            }}
+          <TensorCamera
+            // Standard Camera props
             style={styles.preview}
             type={this.state.type}
+            // Tensor related props
+            cameraTextureHeight={textureDims.height}
+            cameraTextureWidth={textureDims.width}
+            resizeHeight={200}
+            resizeWidth={152}
+            resizeDepth={3}
+            onReady={this.handleCameraStream}
+            autorender={true}
           />
         </View>
       );
