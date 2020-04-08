05992deeb97654851253392d086dde8cfe23266d
shokiami <kiami.sho@gmail.com>
2020-04-08 13:39:24 -0700

    Skeleton updates every loop

--- src/Skeleton.js
+++ src/Skeleton.js
@@ -3,10 +3,7 @@ import {View, Text} from 'react-native';
 import Canvas from 'react-native-canvas';
 import styles from './Styles';
 
-var pose;
-
 export default class Skeleton extends Component {
-  
   render() {
     if (this.props.pose == null) {
       return (
@@ -17,13 +14,12 @@ export default class Skeleton extends Component {
         </View>
       );
     } else {
-      pose = this.props.pose;
       const drawSkeleton = (canvas) => {
         if (canvas != null) {
           const ctx = canvas.getContext('2d');
+          var pose = this.props.pose;
           ctx.fillStyle = 'purple';
-          console.log('hi');
-          //ctx.fillRect(0, 0, 100*pose.score, 100*pose.score);
+          ctx.fillRect(0, 0, 100 * pose.score, 100 * pose.score);
         }
       }
       return (
