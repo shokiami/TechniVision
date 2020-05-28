44216341ece79740ebd756941a47dffdb9f4d2bc
shokiami <kiami.sho@gmail.com>
2020-05-28 01:55:32 -0700

    In the process of trying to get openpose to work

--- src/PoseNet.js
+++ src/PoseNet.js
@@ -10,6 +10,8 @@ import styles from './Styles';
 
 var TensorCamera;
 var model;
+const xsize = 160;
+const ysize = 160;
 
 export default class Posenet extends Component {
   state = {
@@ -18,9 +20,9 @@ export default class Posenet extends Component {
     type: Camera.Constants.Type.front,
     pose: null
   }
-  
+
   async componentDidMount() {
-    //Set up functions
+    //On opening
     this.mounted = true;
     this.handleCameraStream = this.handleCameraStream.bind(this);
     //Ask for camera permission
@@ -29,35 +31,54 @@ export default class Posenet extends Component {
     //Load TensorFlow and PoseNet
     await tf.ready();
     TensorCamera = cameraWithTensors(Camera);
-    console.log("Begining to load model");
     model = await tf.loadGraphModel(bundleResourceIO(require('./openpose/structure.json'), require('./openpose/weights.bin')));
-    console.log("Finished loading model");
-    // model = await posenet.load({
-    //   architecture: 'MobileNetV1',
-    //   outputStride: 16,
-    //   inputResolution: {width: 320, height: 568},
-    //   multiplier: 0.5
-    // });
     this.setState({loaded: true});
   }
-  
+
   async componentWillUnmount() {
-    console.log("unmounting");
+    //On closing
     this.mounted = false;
   }
-  
+
   handleCameraStream(images) {
     var loop = async () => {
-      const nextImageTensor = images.next().value;
       if (this.mounted) {
-        //Convert nextImageTensor into data processible by tensorflow
-        var data = tf.div(tf.stack([nextImageTensor.slice([0,0,0],[160,160,1]),nextImageTensor.slice([0,0,1],[160,160,1]),nextImageTensor.slice([0,0,2],[160,160,1])]).squeeze().expandDims().asType('float32'),255.0);
-        let pose = model.predict(data);
+        //Get tensor for current frame
+        const nextImageTensor = images.next().value;
+        //Convert nextImageTensor into shape required by openpose
+        var data = tf.div(tf.stack([nextImageTensor.slice([0,0,0],[xsize,ysize,1]),nextImageTensor.slice([0,0,1],[xsize,ysize,1]),nextImageTensor.slice([0,0,2],[xsize,ysize,1])]).squeeze().expandDims().asType('float32'),255.0);
+        //Run openpose on data
+        let output = model.predict(data);
+        //Keypoint names
+        const names = [
+          "nose", "neck", "left_shoulder", "left_elbow", "left_wrist", "right_shoulder", "right_elbow", "right_wrist",
+          "center_waist", "left_waist", "left_knee", "left_ankle", "right_waist", "right_knee", "right_ankle",
+          "left_eye", "right_eye", "left_ear", "right_ear",
+          "right_toe", "right_midfoot", "right_heel", "left_toe", "left_midfoot", "left_heel"
+        ];
+        var pose = {}
+        for (let keypoint = 0; keypoint < 24; keypoint ++) {
+          var layer = await output.slice([0,keypoint,0,0], [1,1,xsize/8, ysize/8]).squeeze().array();
+          //console.log(layer);
+          //continue;
+          var max_confidence = 0;
+          var x = 0;
+          var y = 0;
+          for (let i = 0; x < xsize/8; x ++) {
+            for (let j = 0; y < ysize/8; y ++) {
+              //console.log(layer.get(i,j) + "," + max_confidence );
+              if (layer[i][j] > max_confidence) {
+                x = i;
+                y = j;
+                max_confidence = layer[i][j];
+              }
+            }
+          }
+          pose[names[keypoint]] = {"confidence": max_confidence, "x": x, "y": y};
+        }
         console.log(pose);
-        
-        //let pose = await model.estimateSinglePose(nextImageTensor, {flipHorizontal: cameraFlip});
         //this.setState({pose: pose});
-        requestAnimationFrame(loop); //.bind(this);
+        requestAnimationFrame(loop);
       }
     }
     loop();
@@ -90,8 +111,8 @@ export default class Posenet extends Component {
             type={this.state.type}
             cameraTextureHeight={textureDims.height}
             cameraTextureWidth={textureDims.width}
-            resizeHeight={160}
-            resizeWidth={160}
+            resizeHeight={ysize}
+            resizeWidth={xsize}
             resizeDepth={3}
             onReady={this.handleCameraStream}
             autorender={true}
